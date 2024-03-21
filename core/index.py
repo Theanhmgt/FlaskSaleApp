49bafb1b51740f18eb267253363ccebb63a059c5
Theanhmgt <theanhmgt66@gmail.com>
2024-03-21 22:55:02 +0700

    refactor:separate index.html file to specific HTML files

--- core/index.py
+++ core/index.py
@@ -11,15 +11,21 @@ def index():
     cate = request.args.get('cate_id')
     kw = request.args.get('q')
 
-    categories = dao.get_Categories()
     prod = dao.get_Products(cate, kw)
-    return render_template('index.html', categories=categories, prod=prod)
+    return render_template('index.html',  prod=prod)
 
 
 @app.route("/products/<int:id>")
 def product(id):
     product = dao.get_product_by_id(id=id)
     return render_template('product-detail.html', product=product)
+
+
+@app.context_processor
+def common_attributes():
+    return {
+        'categories': dao.get_Categories()
+    }
 
 
 if __name__ == "__main__":
