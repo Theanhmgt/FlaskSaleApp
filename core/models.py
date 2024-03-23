a684236119dc3a66b5e4b9298cfc88d41925dbea
Theanhmgt <theanhmgt66@gmail.com>
2024-03-23 14:21:35 +0700

    feat: add product model, init cate and product data in db

--- core/models.py
+++ core/models.py
@@ -1,14 +1,38 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.orm import relationship
+import json
 from __init__ import db, app
 
 
 class Category(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
+    products = relationship('Product', backref='category', lazy=True)
+
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    price = Column(Float, default=0)
+    photo = Column(String(255))
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
 
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+
+        with open("data/categories.json", encoding="utf-8") as f:
+            data = json.load(f)
+            for p in data:
+                cate = Category(**p)
+                db.session.add(cate)
+            db.session.commit()
+
+
+        with open("data/products.json", encoding="utf-8") as f:
+            prods = json.load(f)
+            for p in prods:
+                prod = Product(**p)
+                db.session.add(prod)
+            db.session.commit()
