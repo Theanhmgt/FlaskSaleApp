94a9d514f861f56019858f70d4b47a71b39a2247
Theanhmgt <theanhmgt66@gmail.com>
2024-03-23 16:37:20 +0700

    feat: add login site in admin context

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,4 @@
-from flask_admin import Admin
+from flask_admin import Admin, expose, BaseView
 from flask_admin.contrib.sqla import ModelView
 from models import *
 from __init__ import app, db
@@ -17,5 +17,12 @@ class ProductModelView(ModelView):
     can_export = True
 
 
+class Stats(BaseView):
+    @expose('/')
+    def index(self):
+        return self.render('admin/stats.html')
+
+
 admin.add_view(CategoryModelView(Category, db.session))
 admin.add_view(ProductModelView(Product, db.session))
+admin.add_view(Stats(name="Thong ke"))
